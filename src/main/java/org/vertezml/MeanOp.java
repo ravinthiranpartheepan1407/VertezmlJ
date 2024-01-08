ed9937da9cb767244103f480bf519295ca74573c
ravinthiranpartheepan1407 <ravinthiranpartheepan1407@gmail.com>
2024-01-08 16:51:02 +0200

    1.0.0: Mean

--- src/main/java/org/vertezml/MeanOp.java
+++ src/main/java/org/vertezml/MeanOp.java
@@ -1,2 +1,12 @@
-package org.vertezml;public class MeanOp {
+package org.vertezml;
+
+public class MeanOp {
+    public static double Mean(double[] values){
+        double sum = 0;
+        int elements;
+        for(elements = 0; elements < values.length; elements++){
+            sum += values[elements];
+        }
+        return sum / values.length;
+    }
 }
